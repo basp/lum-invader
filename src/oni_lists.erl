b4456b66125142c87eee7a8f0536bfc8821ed3ac
Bas Pennings <baspster@gmail.com>
2014-01-04 16:21:13 +0100

    More spec cleanup

--- src/oni_lists.erl
+++ src/oni_lists.erl
@@ -6,14 +6,12 @@
 
 -export([first/2]).
 
--type pred() :: fun((any()) -> boolean()).
-
 %%%============================================================================
 %%% API
 %%%============================================================================
 %% @doc Returns the first element satisfying the predicate.
 %% http://stackoverflow.com/a/12657896
--spec first(pred(), [any()]) -> any() | nothing.
+-spec first(fun((any()) -> boolean()), [any()]) -> any() | nothing.
 first(Pred, List) ->
     case lists:dropwhile(fun(X) -> not Pred(X) end, List) of
         [] -> nothing;
