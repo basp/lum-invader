e26ab09b855fca09a10069d3db896288b6b93fbb
Bas Pennings <baspster@gmail.com>
2014-01-06 19:53:28 +0100

    Removed old tests, added some new ones

--- test/oni_bstr_tests.erl
+++ test/oni_bstr_tests.erl
@@ -17,9 +17,31 @@ join_test() ->
 		oni_bstr:join([<<"\"foo bar\"">>])),
 	?assertEqual(
 		<<"foo and bar">>, 
-		oni_bstr:join([<<"foo", "bar">>], <<", ">>, <<" and ">>)),
+		oni_bstr:join([<<"foo">>, <<"bar">>], <<", ">>, <<" and ">>)),
 	?assertEqual(
 		<<"foo, bar and quux">>, 
 		oni_bstr:join(
 			[<<"foo">>, <<"bar">>, <<"quux">>], 
 			<<", ">>, <<" and ">>)).
+
+starts_with_test() ->
+	?assertEqual(true, oni_bstr:starts_with(<<>>, <<"quux">>)),
+	?assertEqual(true, oni_bstr:starts_with(<<"foo">>, <<"foobar">>)),
+	?assertEqual(true, oni_bstr:starts_with(<<"fo">>, <<"fo quux">>)),
+	?assertEqual(false, oni_bstr:starts_with(<<"fo">>, <<"quux">>)).
+
+match_test() ->
+	?assertEqual(
+		nothing, oni_bstr:match(<<"foo">>, [])),
+	?assertEqual(
+		nothing, oni_bstr:match(<<"foo">>, [<<"bar">>])),
+	?assertEqual(
+		<<"foo">>, oni_bstr:match(<<"foo">>, [<<"foo">>, <<"bar">>])),
+	?assertEqual(
+		<<"bar">>, oni_bstr:match(<<"bar">>, [<<"foo">>, <<"bar">>])),
+	?assertEqual(
+		<<"foo">>, oni_bstr:match(<<"fo">>, [<<"bar">>, <<"foo">>])),
+	?assertEqual(
+		ambiguous, oni_bstr:match(<<"foo">>, [<<"foobar">>, <<"foo">>])),
+	?assertEqual(
+		ambiguous, oni_bstr:match(<<"f">>, [<<"f">>, <<"f">>])).
