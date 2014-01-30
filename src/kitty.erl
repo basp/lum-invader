21240da93d172f95179388beeb21255c977c6395
Bas Pennings <baspster@gmail.com>
2014-01-30 07:20:55 +0100

    Fixed notify bug in test world

--- src/kitty.erl
+++ src/kitty.erl
@@ -1,3 +1,20 @@
+%%%----------------------------------------------------------------------------
+%%% @copyright 2013-2014 Bas Pennings [http://github.com/basp]
+%%% @doc Kitty world implementation.
+%%%
+%%% Permission to use, copy, modify, and/or distribute this software for any
+%%% purpose with or without fee is hereby granted, provided that the above
+%%% copyright notice and this permission notice appear in all copies.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+%%% @end
+%%%----------------------------------------------------------------------------
 -module(kitty).
 
 -compile(export_all).
@@ -37,12 +54,14 @@ init() ->
 %%% Actors
 %%%============================================================================
 alana_loop(Obj) ->
-    Bindings = [{this, Obj}],
+    %% Set player to 0 otherwise notify will fail
+    Bindings = [{this, Obj}, {player, 0}],
     speak(Bindings),
     timer:sleep(4000),
     alana_loop(Obj).
 
 spawn_alana(Obj) ->
+    oni_aq_sup:start_queue(Obj),
     spawn(fun() -> alana_loop(Obj) end).
 
 speak(Bindings) ->
@@ -54,9 +73,29 @@ speak(Bindings) ->
             oni:announce(Location, <<"Prrrr.">>);
         R when R > 0.500 ->
             oni:announce(Location, <<"Alana looks at you pondering.">>);
+        R when R > 0.250 ->
+            Pack = oni_pack:create({kitty, start_gnawing}, Bindings),
+            oni_aq_sup:queue(This, Pack);
         _Other ->
             oni:announce(Location, <<"Miiiiiiiaaaaauuuuwwww!">>)
     end.
+
+start_gnawing(Bindings) ->
+    This = proplists:get_value(this, Bindings),
+    Location = oni_db:location(This),
+    oni:announce(Location, <<"Alana starts gnawing on some cables.">>),
+    {continue, 3000, {kitty, continue_gnawing, [Bindings]}}.
+
+continue_gnawing(Bindings) ->
+    This = proplists:get_value(this, Bindings),
+    Location = oni_db:location(This),
+    oni:announce(Location, <<"Alana continues to gnaw on some cables.">>),
+    {continue, 1500, {kitty, finish_gnawing, [Bindings]}}.
+
+finish_gnawing(Bindings) ->
+    This = proplists:get_value(this, Bindings),
+    Location = oni_db:location(This),
+    oni:announce(Location, <<"Alana finishes gnawing on his cable. He seems satisfied for now.">>).
 
 %%%============================================================================
 %%% Mistress verbs
@@ -66,4 +105,3 @@ look(Bindings) ->
     %% io:format("~p~n", [Bindings]),
     Player = proplists:get_value(player, Bindings),
     oni:notify(Player, "You look around.").
-
